d840db2d92ee53934f7ba568f384c68ac82eb6bf
谷口慈行 <taniguchi@commerble.com>
2018-09-23 16:30:01 +0900

    modify deploy cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,5 @@
 @echo off
 cd %~dp0
+cp %DEPLOYMENT_SOURCE%\Atom %DEPLOYMENT_TARGET%\Atom
+cp %DEPLOYMENT_SOURCE%\host.json %DEPLOYMENT_TARGET%\host.json
 cp %DEPLOYMENT_SOURCE%\proxies.json %DEPLOYMENT_TARGET%\proxies.json
