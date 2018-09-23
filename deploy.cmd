a76f10bf5bc548c87fba25996ca6e9db90717c99
谷口慈行 <taniguchi@commerble.com>
2018-09-23 16:32:47 +0900

    modify deploy cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,4 @@
 @echo off
-cp %DEPLOYMENT_SOURCE%\Atom %DEPLOYMENT_TARGET%\Atom
+xcopy %DEPLOYMENT_SOURCE%\Atom %DEPLOYMENT_TARGET%\Atom /seyiq
 cp %DEPLOYMENT_SOURCE%\host.json %DEPLOYMENT_TARGET%\host.json
 cp %DEPLOYMENT_SOURCE%\proxies.json %DEPLOYMENT_TARGET%\proxies.json
